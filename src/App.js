d1619cc19058929f0cad53f94374a96112c72a5b
Ceri <ceriwoolway@gmail.com>
2023-10-15 18:38:27 +0100

    Basic portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './assets/cherry.png';
-
+import tgaaStatsPreview from './assets/tgaa-stats-preview.webp';
 import './App.css';
+
+const portfolioItems = [
+  {
+    title: 'TGAA stats',
+    description:
+      'A one-page website to show the percentage of players who got each achievement on The Great Ace Attorney Chronicles.',
+    tech: 'Made using React and powered by the Steam Web API',
+    url: 'tgaa-stats',
+    previewImage: tgaaStatsPreview,
+  },
+];
 
 function App() {
   return (
@@ -11,6 +22,9 @@ function App() {
       </header>
       <main>
         <About />
+        <div>
+          <Portfolio />
+        </div>
       </main>
     </div>
   );
@@ -28,5 +42,37 @@ const About = () => (
     </p>
   </div>
 );
+
+const Portfolio = () => {
+  return (
+    <div>
+      {portfolioItems.map((item, index) => (
+        <div key={index}>
+          <PortfolioCard item={item} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const PortfolioCard = ({ item }) => {
+  const url = `https://${item.url}.cherrycodes.net`;
+  const shortUrl = `${item.url}.cherrycodes.net`;
+
+  return (
+    <div className="portfolio-card">
+      <h2>{item.title}</h2>
+      <img src={item.previewImage} alt={item.title} />
+      <div>{item.description}</div>
+      <div>
+        <h3>Techy details</h3>
+        {item.tech}
+      </div>
+      <a href={url} target="_blank" rel="noreferrer">
+        {shortUrl}
+      </a>
+    </div>
+  );
+};
 
 export default App;
