409279ab90757cadd89f37e6656ce5f4f4972aeb
Ceri <ceriwoolway@gmail.com>
2023-10-15 18:07:42 +0100

    Basic layout and content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import logo from './assets/cherry.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="100" />
+        <h1>cherrycodes.net</h1>
       </header>
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
+
+const About = () => (
+  <div>
+    <p>
+      Hi! I am a software engineer based in the south west of the UK. I started my career as a web designer, before
+      moving on to front end development, and am now dipping my toes into full stack and app development.
+    </p>
+    <p>
+      In my spare time I like to combine my coding skills with some of the geeky things I'm into. This little corner of
+      the internet is where I showcase it all.
+    </p>
+  </div>
+);
 
 export default App;
